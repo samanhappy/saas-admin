b51ba6fd74a13069160c286a48acc0c44d4db18d
samanhappy <samanhappy@gmail.com>
2017-12-29 18:02:05 +0800

    base64

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import ManageParty from '@/components/manage/party'
 import VueResource from 'vue-resource'
+import { Base64 } from 'js-base64'
 
 Vue.use(VueResource)
 Vue.use(Router)
@@ -10,6 +11,8 @@ Vue.use(Router)
 Vue.http.interceptors.push(function (request, next) {
   if (this.config.user.token) {
     request.headers.set('userToken', this.config.user.token)
+    request.headers.set('appId', this.config.appId)
+    request.headers.set('type', this.config.type)
   } else if (this.$route.query.token && this.$route.query.appId && this.$route.query.type) {
     this.config.token = this.$route.query.token
     this.config.appId = this.$route.query.appId
@@ -30,7 +33,7 @@ Vue.http.interceptors.push(function (request, next) {
   next(function (response) {
     this.config.fullLoading = false
     if (response.headers.get('user')) {
-      this.config.user = JSON.parse(response.headers.get('user'))
+      this.config.user = JSON.parse(Base64.decode(response.headers.get('user')))
     }
     if (response.status !== 200) {
       this.$message.error('请求出错')
